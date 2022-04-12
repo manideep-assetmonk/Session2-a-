7d07ac65732a8643920323dcbbe6a9e234dff030
manideep-assetmonk <103191212+manideep-assetmonk@users.noreply.github.com>
2022-04-12 14:52:11 +0530

    count-to-06

--- resetcopy.js
+++ resetcopy.js
@@ -3,9 +3,13 @@ module.exports=function(a,b){
     let d='!';
     if(b==undefined)
     {
+//         if error value is undefined then string should repeat upto length d
         return a+d.repeat(c);
     }
     else{
+/**
+* repeat the string upto given value .
+**/
         return a+d.repeat(b);
     }
     
