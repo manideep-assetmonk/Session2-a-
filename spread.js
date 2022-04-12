23f227d135aa7410be2ef7b4b1ce6722a72fbbea
manideep-assetmonk <103191212+manideep-assetmonk@users.noreply.github.com>
2022-04-12 14:54:15 +0530

    count-to-06

--- spread.js
+++ spread.js
@@ -1,3 +1,6 @@
+/**
+* process.argv property return and array containing the arguments passing to the process when it run in comand line.
+**/
 let a=process.argv.slice(2);
 var min=Math.min(...a)
 
